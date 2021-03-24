42db8e18480de3becf0d2e9e13296fb5ae63cbf6
unknown <hugodinis@ua.pt>
2021-03-24 23:56:00 +0000

    lab2 Ex 2 e 3 falta completar um pouco mais as soluções

---
+++ lab2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -0,0 +1,59 @@
+package geocoding;
+
+import connection.ISimpleHttpClient;
+import exceptions.BadArrayIndex;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AddressResolverTest {
+    private AddressResolver resolver;
+    private ISimpleHttpClient httpClient;
+
+    @Test
+    void whenGoodAlboiGps_returnAddress() throws ParseException, IOException, URISyntaxException, BadArrayIndex {
+
+
+        String jsonResponse = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-1812156666\",\"roadMetadata\":null}]}]}";
+        String url = "http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&location=40.640661%2C-8.656688&includeRoadMetadata=true";
+
+        httpClient = mock(ISimpleHttpClient.class);
+        resolver = new AddressResolver(httpClient);
+
+        when(httpClient.get(url)).thenReturn(jsonResponse);
+
+        //test
+        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
+
+        //return
+        assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
+    }
+
+    @Test
+    public void whenBadCoordidates_throwBadArrayindex() throws IOException, URISyntaxException, ParseException {
+
+        httpClient = mock(ISimpleHttpClient.class);
+        resolver = new AddressResolver(httpClient);
+
+        String jsonResponse = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":-190.0,\"lng\":-190.0}},\"locations\":[]}]}";
+        String url = "http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&location=-190.000000%2C-190.000000&includeRoadMetadata=true";
+
+        when(httpClient.get(url)).thenReturn(jsonResponse);
+        //test
+        assertThrows(BadArrayIndex.class,() -> resolver.findAddressForLocation(-190,-190));
+    }
+}
---
+++ lab2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -0,0 +1,41 @@
+package integration;
+
+import connection.ISimpleHttpClient;
+import connection.TqsBasicHttpClient;
+import exceptions.BadArrayIndex;
+import geocoding.Address;
+import geocoding.AddressResolver;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AddressResolverIT {
+
+    private AddressResolver resolver;
+    private ISimpleHttpClient httpClient;
+
+    @BeforeEach
+    public void init(){
+        httpClient = new TqsBasicHttpClient();
+        resolver = new AddressResolver(httpClient);
+    }
+
+    @Test
+    public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException, BadArrayIndex {
+        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
+        assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
+
+    }
+
+    @Test
+    public void whenBadCoordidates_throwBadArrayindex() throws IOException, URISyntaxException, ParseException {
+        assertThrows(BadArrayIndex.class,() -> resolver.findAddressForLocation(-190,-190));
+    }
+
+}
